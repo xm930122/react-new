677f9bec333e77b19fed0e875a257cc3828dfffa
xiemin <xiemin863533@163.com>
2025-12-15 14:07:37 +0800

    tree file add

---
+++ src/utils/tree.js
@@ -0,0 +1,27 @@
+/**
+ * listArr
+ * parentKey:父key
+ * key:子key
+*/
+export const listToTree = (listArr, parentKey, key) => {
+    const tree = [];
+    const map = new Map();
+    listArr.forEach(item => map.set(item[key], {...item}));
+    listArr.forEach(item => {
+        const node = map.get(item[key]);
+        // parentKey为0代表根结点
+        if (item[parentKey] === 0) {
+            tree.push(node);
+        } else {
+            // 如果不是根结点，找到对应的父节点
+            const parent = map.get(item[parentKey]);
+            if (parent) {
+                if (!parent.childern) {
+                    parent.childern = [];
+                }
+                parent.childern.push(node);
+            }
+        }
+    })
+    return tree;
+}
